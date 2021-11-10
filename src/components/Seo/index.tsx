83890a121681303025ba48ef2d63f2d16a6bca10
Pedro Lucas Padilha Canassa <pedro.canassa@amedigital.com>
2021-11-09 23:20:17 -0300

    ajusta seo do aplicação

---
+++ src/components/Seo/index.tsx
@@ -0,0 +1,25 @@
+
+export const Seo = () => {
+  const title = 'Selic Hoje: Meta para a taxa Selic'
+  const description = '​A Selic é a taxa básica de juros da economia. É o principal instrumento de política monetária utilizado pelo Banco Central (BC) para controlar a inflação.'
+  const url = 'https://selichoje.com/'
+  const image = 'selichoje.webp'
+
+  return (
+    <>
+      <meta name="title" content={title} />
+      <meta name="description" content={description} />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={image} />
+    </>)
+}
