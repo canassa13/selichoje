92f3680efec5c3643bd0b02e03ce451d259c108d
Pedro <pedro_canassa@hotmail.com>
2022-03-17 08:43:25 -0300

    feat: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,29 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 
-import { Header, Footer, Seo } from "../components/";
+import { Header, Footer, Seo, Analytics } from "../components/";
+import * as gtag from "../services/gaTrackingId";
 
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Seo />
+      <Analytics />
       <Header />
       <Component {...pageProps} />
       <Footer />
